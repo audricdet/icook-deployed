298929a44b3aac135a53b0eb82b13e10570bf295
audricdet <audric.det@hotmail.com>
2023-02-23 14:23:20 +0100

    only one ingredient input working

--- controllers/getRecipesbyIngredients.mjs
+++ controllers/getRecipesbyIngredients.mjs
@@ -1,27 +1,3 @@
-// const getRecipesByIngredients = async (request, response) => {
-//     const { ingredient_1, ingredient_2, ingredient_3, number_of_recipe } = request.body
-
-//     try {
-//         const res = await fetch (`https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredient_1},+${ingredient_2},+${ingredient_3}&number=${number_of_recipe}&apiKey=${process.env.API_KEY_SPOONACULAR}`)
-//         const result = await res.json()
-
-//         const filteredResult = result.map(({ id, title, image, missedIngredients, usedIngredients }) => {
-//             const score = usedIngredients.length;
-//             return { id, title, image, missedIngredients, usedIngredients, score };
-//         });
-
-//           // Trier les résultats par score décroissant
-//         filteredResult.sort((a, b) => b.score - a.score);
-
-//         console.log(filteredResult);
-//         response.send({ result: filteredResult });
-//     } catch (error) {
-//         console.error(error)
-//         response.status(500).send({ error: "Could not fetch recipes" })
-//     }
-// }
-
-// export default getRecipesByIngredients
 
 const getRecipesByIngredients = async (request, response) => {
     // Récupérer le tableau d'ingrédients et le nombre de recettes souhaité depuis le corps de la requête
