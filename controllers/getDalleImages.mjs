531a037a5d36aa0b658c6d7df91000a2d599457a
audricdet <audric.det@hotmail.com>
2023-02-28 14:13:47 +0100

    dalle test 1

--- controllers/getDalleImages.mjs
+++ controllers/getDalleImages.mjs
@@ -1,5 +1,5 @@
-import bodyParser from "body-parser"
 import { Configuration, OpenAIApi } from "openai"
+import bodyParser from 'body-parser'
 
 const apiKey = process.env.API_KEY_OPENAI
 
@@ -8,20 +8,18 @@ const configuration = new Configuration({
 })
 const openai = new OpenAIApi(configuration)
 
-const getDalleImages = async (request) => {
-    const { title } = request.body
+const getDalleImages = async (request, response) => {
+    const title = request.body.title
 
     const response = await openai.createImage({
-        prompt: [title],
+        prompt: title,
         n: 1,
         size: "256x256",
     });
-console.log(response.data)
+    console.log(response.data)
 
-return response.data
-
+    response.send(response.data[0].url)
 }
 
-getDalleImages()
-
 export default getDalleImages
+
